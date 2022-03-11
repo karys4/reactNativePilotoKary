d16a2baa654af832ef642e364f1c3ea1bbf894ef
karyrodriguez <kary.github@gmail.com>
2022-03-11 13:54:03 -0600

    Agregar imagen y estilos a diversos componentes(botones, link, imagen)

--- views/loginForm/LoginForm.js
+++ views/loginForm/LoginForm.js
@@ -1,47 +1,84 @@
 import React from "react";
+//import { Linking } from "react-native";
 import {
   StyleSheet,
   View,
   Text,
   TextInput,
   Button,
-  Keyboard,
+  TouchableOpacity,
+  Linking,
+  Image,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-export default function LoginForm() {
+export default function LoginForm({ text }) {
   return (
     //Si es más de un elemento, debe ir dentro de un View.
-    <View style={styles.container}>
-      <Text style={styles.title}>Pantalla de iniciar sesión</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Correo Electrónico"
-        autoCapitalize="none"
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Contraseña"
-        autoCapitalize="none"
-        secureTextEntry={true}
-      />
-      <Button
-        style={styles.boton}
+    <SafeAreaView backgroundColor="#6f4a8e">
+      <View style={styles.container}>
+        {/*<Text style={styles.title}>Pantalla de iniciar sesión</Text>*/}
+        <Image
+          style={styles.imagen}
+          source={require("../../assets/Logo.png")}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Correo Electrónico"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Contraseña"
+          autoCapitalize="none"
+          secureTextEntry={true}
+        />
+
+        {/*<Button
+        //style={styles.boton}
         title="Enviar"
+        color="#221f3b"
         onPress={() => console.log("Formulario iniciar sesión enviado")}
       />
+
       <Button
         style={styles.boton}
         title="Registrarse"
+        color="#221f3b"
+        marginTop
         onPress={() => console.log("Formulario de registro enviado")}
-      />
-    </View>
+      />*/}
+
+        <TouchableOpacity style={styles.boton}>
+          <Text style={styles.titleButton}>{text}Iniciar sesión</Text>
+        </TouchableOpacity>
+
+        <Text
+          style={{
+            color: "white",
+            fontSize: 15,
+            margin: 20,
+            textAlign: "center",
+            textDecorationLine: "underline",
+          }}
+          onPress={() => Linking.openURL("http://google.com")}
+        >
+          Olvidaste la contraseña
+        </Text>
+        <TouchableOpacity style={styles.boton}>
+          <Text style={styles.titleButton}>{text}Registrarse</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 //Agregando estilos a los componentes
 const styles = StyleSheet.create({
   container: {
-    //backgroundColor: "#6f4a8e",
+    backgroundColor: "#6f4a8e",
+    alignItems: "center",
+    marginTop: 80,
   },
   title: {
     textAlign: "center",
@@ -51,17 +88,34 @@ const styles = StyleSheet.create({
     marginBottom: 15,
   },
   input: {
-    height: 40,
+    height: 60,
+    width: 365,
     margin: 12,
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+    backgroundColor: "white",
   },
   boton: {
-    height: 40,
-    margin: 12,
+    height: 60,
+    width: 365,
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
     borderWidth: 1,
     padding: 10,
     borderRadius: 10,
+    backgroundColor: "#221f3b",
+    //alignItems: "center",
+  },
+  titleButton: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 16,
+  },
+  imagen: {
+    alignContent: "center",
+    justifyContent: "center",
   },
 });
